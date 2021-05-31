07949a3e6c58af60a592ed189f9887881d1eb288
alex-killer <onefrank@bk.ru>
2021-05-31 22:56:38 +0700

    31.05.21

--- resources/views/blog/articles.blade.php
+++ resources/views/blog/articles.blade.php
@@ -5,10 +5,13 @@
 @endsection
 
 @section('content')
+    <ul>
     @foreach ($articles as $article)
+
         <li>
             <a href="{{ route("blog_article", ["article" => $article]) }}">{{ $article->id }}. {{ $article->title }} </a>
         </li>
 
     @endforeach
+    </ul>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,26 +5,51 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>@yield('title')</title>
 </head>
 <body>
-<ul>
-    <li>
-        <a href="{{ route("main") }}">Главная страница</a>
-    </li>
-    <li>
-        <a href="{{ route("contacts") }}">Контакты</a>
-    </li>
-    <li>
-        <a href="{{ route("info") }}">Информация</a>
-    </li>
-    <li>
-        <a href="{{ route("blog_categories") }}">Категории</a>
-    </li>
-    <li>
-        <a href="{{ route("blog_articles") }}">Новости</a>
-    </li>
-</ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a href="{{ route("main") }}" class="navbar-brand">My Blog</a>
+
+            {{-- Кнопка гамбургер будет отображаться только в мобильной версии --}}
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+                aria-controls="navbarContent" aria-expanded="false"> {{-- false значит меню закрыто --}}
+                <span class="navbar-toggler-icon"></span> {{-- Иконка нашего меню --}}
+            </button>
+
+            {{-- разворачивающиеся меню при нажатии на кнопку --}}
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a href="{{ route("main") }}" class="nav-link">Главная страница</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route("contacts") }}" class="nav-link">Контакты</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route("info") }}" class="nav-link">Информация</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route("blog_categories") }}" class="nav-link">Категории</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route("blog_articles") }}" class="nav-link">Новости</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input type="search" placeholder="Search" class="form-control me-2">
+                    <button class="btn btn-outline-success">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+<div>
 @yield('content')
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</div>
 </body>
 </html>
